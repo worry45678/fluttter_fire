54441b5c46565c3a70e5db1738e9f2d75dc6cfd0
worry45678 <worry45678@126.com>
2019-05-21 06:51:12 +0800

    login

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,8 @@ void getHttp() async {
     print(response);
     final body = json.decode(response.toString());
     final records3 = body['data'].map((rec) {
-      return new Record(date: null, name: rec['name'], isCheck: rec['isCheck']);
+      return new Record(
+          date: rec['date'], name: rec['name'], isCheck: rec['isCheck']);
     }).toList();
     print(records3);
   } catch (e) {
@@ -33,7 +34,7 @@ class FirstScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Scaffold(
         appBar: new AppBar(
-          title: new Text('First Screen'),
+          title: new Text('灭火器、消火栓点检系统'),
         ),
         body: new ListView(
           children: <Widget>[
@@ -46,16 +47,6 @@ class FirstScreen extends StatelessWidget {
                 );
               },
             ),
-            new RaisedButton(
-              child: new Text('扫码界面'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  new MaterialPageRoute(builder: (context) => new ScanQr()),
-                );
-              },
-            ),
-            new RaisedButton(child: new Text('获取数据'), onPressed: getHttp),
           ],
         ));
   }
@@ -64,17 +55,194 @@ class FirstScreen extends StatelessWidget {
 class Record {
   const Record({this.name, this.date, this.isCheck});
   final String name;
-  final DateTime date;
+  final String date;
   final bool isCheck;
 }
 
-final records2 = <Record>[
-  new Record(name: '灭火器1', date: null, isCheck: true),
-  new Record(name: '灭火器2', date: null, isCheck: true),
-  new Record(name: '灭火器3', date: null, isCheck: true),
-  new Record(name: '灭火器4', date: null, isCheck: false),
-  new Record(name: '灭火器5', date: null, isCheck: false),
-];
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool _phoneState, _pwdState = false;
+  String _checkStr;
+  TextEditingController _phonecontroller = new TextEditingController();
+  TextEditingController _pwdcontroller = new TextEditingController();
+
+  void _checkPhone() {
+    if (_phonecontroller.text.isNotEmpty &&
+        _phonecontroller.text.trim().length == 11) {
+      _phoneState = true;
+    } else {
+      _phoneState = false;
+    }
+  }
+
+  void _checkPwd() {
+    if (_pwdcontroller.text.isNotEmpty &&
+        _pwdcontroller.text.trim().length >= 6 &&
+        _pwdcontroller.text.trim().length <= 10) {
+      _pwdState = true;
+    } else {
+      _pwdState = false;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: '轻签到',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('极速登录'),
+        ),
+        body: new ListView(
+          children: <Widget>[
+            new Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                new Padding(
+                    padding: new EdgeInsets.all(30.0),
+                    child: new Image.asset(
+                      'images/ic_launcher.png',
+                      scale: 1.2,
+                    )),
+                new Padding(
+                  padding: new EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 15.0),
+                  child: new Stack(
+                    alignment: new Alignment(1.0, 1.0),
+                    //statck
+                    children: <Widget>[
+                      new Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            new Padding(
+                              padding:
+                                  new EdgeInsets.fromLTRB(0.0, 0.0, 5.0, 0.0),
+                              child: new Image.asset(
+                                'images/icon_username.png',
+                                width: 40.0,
+                                height: 40.0,
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                            new Expanded(
+                              child: new TextField(
+                                controller: _phonecontroller,
+                                keyboardType: TextInputType.phone,
+                                decoration: new InputDecoration(
+                                  hintText: '请输入用户名',
+                                ),
+                              ),
+                            ),
+                          ]),
+                      new IconButton(
+                        icon: new Icon(Icons.clear, color: Colors.black45),
+                        onPressed: () {
+                          _phonecontroller.clear();
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                new Padding(
+                  padding: new EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 40.0),
+                  child: new Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        new Padding(
+                          padding: new EdgeInsets.fromLTRB(0.0, 0.0, 5.0, 0.0),
+                          child: new Image.asset(
+                            'images/icon_password.png',
+                            width: 40.0,
+                            height: 40.0,
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                        new Expanded(
+                          child: new TextField(
+                            controller: _pwdcontroller,
+                            decoration: new InputDecoration(
+                              hintText: '请输入密码',
+                              suffixIcon: new IconButton(
+                                icon: new Icon(Icons.clear,
+                                    color: Colors.black45),
+                                onPressed: () {
+                                  _pwdcontroller.clear();
+                                },
+                              ),
+                            ),
+                            obscureText: true,
+                          ),
+                        ),
+                      ]),
+                ),
+                new Container(
+                  width: 340.0,
+                  child: new Card(
+                    color: Colors.blue,
+                    elevation: 16.0,
+                    child: new FlatButton(
+                      child: new Padding(
+                        padding: new EdgeInsets.all(10.0),
+                        child: new Text(
+                          '极速登录',
+                          style: new TextStyle(
+                              color: Colors.white, fontSize: 16.0),
+                        ),
+                      ),
+                      onPressed: () {
+                        _checkPhone();
+                        _checkPwd();
+                        if (_phoneState && _pwdState) {
+                          _checkStr = '页面跳转下期见咯！';
+                        } else {
+                          if (!_phoneState) {
+                            _checkStr = '请输入11位手机号！';
+                          } else if (!_pwdState) {
+                            _checkStr = '请输入6-10位密码！';
+                          }
+                        }
+                        print(_checkStr);
+                        showDialog<Null>(
+                          context: context,
+                          barrierDismissible: false,
+                          child: new AlertDialog(
+                            title: new Text(
+                              '温馨提示',
+                              style: new TextStyle(
+                                color: Colors.black54,
+                                fontSize: 18.0,
+                              ),
+                            ),
+                            content: new Text(_checkStr),
+                            actions: <Widget>[
+                              new FlatButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: new Text('确定')),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
 class ShoppingListItem extends StatelessWidget {
   ShoppingListItem({Record record})
@@ -108,8 +276,7 @@ class ShoppingListItem extends StatelessWidget {
         child: new Text(record.name[0]),
       ),
       title: new Text(record.name, style: _getTextStyle(context)),
-      trailing: new Text('${record.date.hour}:${record.date.minute}',
-          style: _getTextStyle(context)),
+      trailing: new Text(record.date, style: _getTextStyle(context)),
     );
   }
 }
@@ -129,9 +296,7 @@ class CheckListState extends State<CheckList> {
       final body = json.decode(response.toString());
       final records3 = body['data'].map((rec) {
         return new Record(
-            date: DateTime.parse(rec['date']),
-            name: rec['name'],
-            isCheck: rec['isCheck']);
+            date: rec['date'], name: rec['name'], isCheck: rec['isCheck']);
       }).toList();
       setState(() {
         this.records = records3;
